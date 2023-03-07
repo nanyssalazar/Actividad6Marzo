da9cd534f07e4d73583f1bae1faa61a773e54c40
ISND 8vo ISND89 9no Semestre <ISND89@AISND20-03.campus.iest.edu.mx>
2023-03-06 18:57:48 -0600

    Added views

---
+++ app/src/main/java/edu/iest/actividad6marzo/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.iest.actividad6marzo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
